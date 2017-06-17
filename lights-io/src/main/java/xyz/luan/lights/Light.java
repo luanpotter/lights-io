eb909d66cf848a409fcf7136482e1a2db9d260ac
Luan Nico <luanpotter27@gmail.com>
2017-06-17 18:14:12 -0300

    Fixes

--- lights-io/src/main/java/xyz/luan/lights/Light.java
+++ lights-io/src/main/java/xyz/luan/lights/Light.java
@@ -1,9 +1,14 @@
 package xyz.luan.lights;
 
+import io.yawp.repository.IdRef;
 import io.yawp.repository.annotations.Endpoint;
+import io.yawp.repository.annotations.Id;
 
 @Endpoint(path = "/lights")
 public class Light {
+
+    @Id
+    private IdRef<Light> id;
 
     private String desciption;
 
--- lights-io/src/main/java/xyz/luan/lights/Switch.java
+++ lights-io/src/main/java/xyz/luan/lights/Switch.java
@@ -2,10 +2,12 @@ package xyz.luan.lights;
 
 import io.yawp.repository.IdRef;
 import io.yawp.repository.annotations.Endpoint;
+import io.yawp.repository.annotations.Id;
 
 @Endpoint(path = "/switches")
 public class Switch {
 
+    @Id
     private IdRef<Switch> id;
 
     private IdRef<Light> lightId;
