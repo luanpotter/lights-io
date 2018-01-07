82bf7aaec48ebc4d4ae7410f15b299d666971219
Luan Nico <luanpotter27@gmail.com>
2018-01-07 16:34:39 -0200

    implementing speech to lights-io

--- speech/src/main/java/xyz/luan/lights/speech/Main.java
+++ speech/src/main/java/xyz/luan/lights/speech/Main.java
@@ -1,5 +1,9 @@
 package xyz.luan.lights.speech;
 
+import xyz.luan.facade.HttpFacade;
+import xyz.luan.facade.Response;
+
+import java.io.IOException;
 import java.io.InputStream;
 
 public class Main {
@@ -7,6 +11,7 @@ public class Main {
     private static final int SAMPLE_RATE = 16000;
 
     public static void main(String[] args) throws Exception {
+        String switchUrl = args[0];
         InputStream credentialFile = Main.class.getResourceAsStream("/lights-io-key.json");
 
         Microphone mic = new Microphone(SAMPLE_RATE);
@@ -15,14 +20,32 @@ public class Main {
         mic.listen(api::speak);
 
         api.listen(text -> {
-            System.out.println("> " + text);
-            if (text.contains("exit")) {
-                System.out.println("I heard 'exit'. It's time to say goodbye o/");
-                mic.close();
-                api.close();
+            try {
+                System.out.println("> " + text);
+                if (text.contains("lights on")) {
+                    put(switchUrl + "/toggle?s=on");
+                } else if (text.contains("lights off")) {
+                    put(switchUrl + "/toggle?s=off");
+                } else if (text.contains("lights")) {
+                    put(switchUrl + "/toggle");
+                } else if (text.contains("exit")) {
+                    System.out.println("I heard 'exit'. It's time to say goodbye o/");
+                    mic.close();
+                    api.close();
+                }
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         });
 
         mic.start();
+    }
+
+    private static void put(String url) throws IOException {
+        Response put = new HttpFacade(url).body("").put();
+        if (put.status() != 200) {
+            System.err.println("Error performing request; status: " + put.status());
+            System.err.println(put.content());
+        }
     }
 }
