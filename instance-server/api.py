1d87615498040cc5cccb3b61b831a7aecb77474f
Luan Nico <luanpotter27@gmail.com>
2020-02-02 13:25:34 -0500

    .

--- instance-server/api.py
+++ instance-server/api.py
@@ -1,5 +1,7 @@
 import flask
+import subprocess
 
+home="/home/enigmatica2-expert-server/enigmatica"
 app = flask.Flask(__name__)
 
 @app.route('/api/status', methods=['GET'])
@@ -8,6 +10,15 @@ def status():
 
 @app.route('/api/stop', methods=['POST'])
 def stop():
-    return "TODO impl stop"
+    subprocess.run(home + "/stop.sh")
+    while True:
+        print("Waiting for the server to quit...")
+        result = subprocess.run(home + "/is_running.sh").returncode
+        if (result == 1):
+            print("Server finished")
+            break
+        time.sleep(10)
+    subprocess.run(home + "/quit.sh", stdin=None, stdout=None, stderr=None)
+    return "Stopped"
 
 app.run(host="0.0.0.0", port=8080)
